92fb74686e170fbe40bad02c0df1f1187d3fc80e
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2023-12-19 14:03:42 -0300

    :sparkles: feat: comentarios norteadores

--- POOkemon-main/Pokemon/Ataque.java
+++ POOkemon-main/Pokemon/Ataque.java
@@ -1,16 +1,18 @@
 public class Ataque {
+    // Encapsulamento - * atributos privados *
     private String nome;
     private int dano;
 
+    // Construtor da classe * Ataque *
     public Ataque(String nome, int dano) {
         this.nome = nome;
         this.dano = dano;
     }
 
+    // getters da classe * ataque *
     public int getDano() {
         return dano;
     }
-
     public String getNome() {
         return nome;
     }
